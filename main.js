fe67ae61a406dd4b9fc2e7fd1a4b0ec9b3b5bcce
kevin breid <kevinbreid@yahoo.com>
2022-07-12 12:47:28 -0700

    commit message

--- main.js
+++ main.js
@@ -4,6 +4,67 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// span.likeglyph.addEventListener('Click', mimicServerCall)
+// .then(res => res.json())
+
+
+const hearts = document.querySelectorAll('.like-glyph')
+
+for (let heart of hearts) {
+  console.log(heart)
+
+  heart.addEventListener('click', heartClickHandler)
+
+}
+
+function heartClickHandler(e){
+  mimicServerCall()
+.then((data) => resolve(data, e.target))
+.catch(reject)
+}
+
+
+
+
+function reject(errMessage){
+  console.log(errMessage)
+  document.getElementById('modal')
+  .removeAttribute('hidden')
+  document.getElementById('modal-message').innerText = errMessage
+  setTimeout(returnHidden, 3000)
+
+  function returnHidden(){
+    document.getElementById('modal')
+    .setAttribute('hidden','hidden')
+  }
+}
+
+
+
+function resolve(data, heart){  
+
+  heart.innerText = '♥'
+      heart.setAttribute('class', 'activated-heart')
+
+    // if (heart.target === EMPTY_HEART){
+    //   heart.innerText = '♥'
+    //   heart.setAttribute('class', 'activated-heart')
+
+    // }
+
+    // else if (heart.target === FULL_HEART){
+
+    //   heart.innerText = EMPTY_HEART
+    //   heart.removeAttribute('class', 'activated-heart')
+
+    // }
+
+    
+
+  }
+  
+
+
 
 
 
